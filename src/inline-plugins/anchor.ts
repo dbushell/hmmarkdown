1bb16ece03564787f132e9483458d3156ea52bd6
David Bushell <david@dbushell.com>
2024-08-28 17:26:08 +0100

    fix anchor matches

--- src/inline-plugins/anchor.ts
+++ src/inline-plugins/anchor.ts
@@ -3,9 +3,53 @@ import {renderInline} from '../render.ts';
 import {replace} from '../utils.ts';
 import {escape} from '../vendor/std-html.ts';
 
-// const REGEXP = /\[([^\]]+)\]\(([^()\s]+)\)/g;
-// const REGEXP = /\[(.+?)\]\(([^()\s]+)\)/g;
 const REGEXP = /(?<!\!)\[(.+?)\]\(([^()\s]+)\)/g;
+
+/*
+
+Okay hear me out...
+Give the following markdown line:
+
+This [emoji] 🍋‍🟩 is a [lime 🍋‍🟩](https://emojipedia.org/lime) ok?
+
+REGEXP will capture too much: "[emoji".."lime)"
+My solution is to walk backgrounds through the matched characters.
+I count the nested square bracket groups and end on zero.
+Parentheses in URLs must be URL encoded.
+
+Acceptable fails:
+  * Anchor `text` has uneven square brackets inside
+  * Anchor `href` has un-encoded parentheses
+
+*/
+const reduceMatch = (match: RegExpExecArray) => {
+  let anchor = '';
+  let href = '';
+  let text = '';
+  let startHref = false;
+  let startText = false;
+  for (let end = -1, i = match[0].length - 1; i >= 0; i--) {
+    const char = match[0][i];
+    anchor += char;
+    if (startHref) href += char;
+    if (startText) text += char;
+    if (!startText) {
+      if (char === ')') startHref = true;
+      if (char === '(') {
+        startHref = false;
+        startText = true;
+      }
+    }
+    if (char === ']') end = end === -1 ? 1 : end + 1;
+    if (char === '[') end--;
+    if (end === 0) break;
+  }
+  return {
+    anchor: [...anchor].reverse().join(''),
+    href: [...href.slice(0, -1)].reverse().join(''),
+    text: [...text.slice(1, -1)].reverse().join('')
+  };
+};
 
 const inlinePlugins = ['emphasis', 'strong'];
 
@@ -13,7 +57,8 @@ const plugin: InlinePlugin = {
   type: 'anchor',
   render: async (out: string, options: HmmOptions) => {
     for (const match of out.matchAll(REGEXP)) {
-      const {0: anchor, 1: text, 2: href} = match;
+      const reduced = reduceMatch(match);
+      const {anchor, href, text} = reduced;
       const props = {text, attributes: {href}};
       props.text = await renderInline(props.text, {
         ...options,
