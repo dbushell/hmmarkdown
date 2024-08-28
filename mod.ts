6a0ec0d4ed8011900dd96a34b8a270da47dda273
David Bushell <david@dbushell.com>
2024-08-28 08:57:35 +0100

    add README badges

--- mod.ts
+++ mod.ts
@@ -1,2 +1,8 @@
-export type * from './src/types.ts';
 export {defaultOptions, hmmarkdown, hmmarkdown as default} from './src/mod.ts';
+export type {
+  HmmBlock,
+  HmmNode,
+  BlockPlugin,
+  InlinePlugin,
+  HmmOptions
+} from './src/types.ts';
